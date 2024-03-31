466e72e0151dcb0082ae7f5153e4e67510334c3d
Tushar <tusharmohan0416@gmail.com>
2024-03-31 19:06:57 +0530

    All projects are now active

--- javascript/home.js
+++ javascript/home.js
@@ -1,1 +1,7 @@
 console.log("js connected");
+
+const myPortolioBtn = document.querySelector("#my-portfolio-link");
+
+myPortolioBtn.addEventListener("click", () => {
+    alert("You are already on my Portfolio Website. Hope you find it beautiful.")
+});
